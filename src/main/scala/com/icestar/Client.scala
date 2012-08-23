807d29390906f6ac32de205ab1410bd0da065a56
IceStar <yubingxing123@gmail.com>
2012-08-23 18:32:43 +0800

    Modify client

--- src/main/scala/com/icestar/Client.scala
+++ src/main/scala/com/icestar/Client.scala
@@ -1,42 +1,17 @@
 package com.icestar
+
 import org.slf4j.LoggerFactory
 
-import com.typesafe.config.ConfigFactory
-
-import akka.actor.actorRef2Scala
-import akka.actor.Actor
 import akka.actor.ActorSystem
-import akka.actor.Props
 import akka.zeromq.Connect
-import akka.zeromq.Frame
-import akka.zeromq.Listener
 import akka.zeromq.SocketType
-import akka.zeromq.ZMQMessage
+import akka.zeromq.zeromqSystem
 
 object Client {
   private val logger = LoggerFactory.getLogger(getClass)
-  def apply(system: ActorSystem, address: String) = {
-    logger.info("Creating Sockets...")
-    val client = system.actorOf(Props(new Client(address)), "Client")
-    logger.info("Socket created success.")
-    client
-  }
-
-  def test() = {
-    val system = ActorSystem("apnclient")
-    val conf = ConfigFactory.load()
-    val client = Client(system, conf.getString("apnclient.address"))
-    client ! """set hahahaha::{"test":"testesttest"}"""
-    system.shutdown()
-  }
-}
-
-class Client(address: String) extends Actor {
-  private val request = context.system.newSocket(SocketType.Req, Connect(address), Listener(self))
-
-  def receive = {
-    case m: String =>
-      println("[Send]:: " + m)
-      request ! ZMQMessage(Seq(Frame(m)))
+  private val system = ActorSystem("apnclient")
+  def apply() = {
+    val actor = system.newSocket(SocketType.Req, Connect(Server.conf.getString("apnserver.address")))
+    actor
   }
 }
