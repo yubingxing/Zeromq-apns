84adc1a0d41adda07b6bc3d619a98515f0bf1590
IceStar <yubingxing123@gmail.com>
2012-08-25 16:37:39 +0800

    modify autogenZMQonUbuntu.sh

--- autogenZMQonUbuntu.sh
+++ autogenZMQonUbuntu.sh
@@ -3,7 +3,7 @@ sudo aptitude install -y python-software-properties
 sudo add-apt-repository ppa:webupd8team/java
 sudo add-apt-repository ppa:chris-lea/zeromq
 sudo aptitude update
-sudo aptitude install -y oracle-java7-installer oracle-java7-jdk oracle-java7-jre
+sudo aptitude install -y oracle-java7-installer 
 sudo aptitude install -y libzmq-dev
 sudo aptitude install -y daemon
 
